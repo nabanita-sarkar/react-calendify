cfd1b8b9c89516e5a3421eef04cada85262227b5
Nabanita Sarkar <nabanita.sarkar1418@gmail.com>
2022-05-09 22:14:59 +0530

    build working

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/main.tsx"),
+      name: "react-calendify",
+      fileName: (format) => `react-calendify.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "react",
+        },
+      },
+    },
+  },
   plugins: [checker({ typescript: true }), react()],
 });
